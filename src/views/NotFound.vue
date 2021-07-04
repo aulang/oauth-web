f8d6398425847158755888abdde76f5841f39b5c
Aulang <aulang@qq.com>
2021-07-04 11:53:29 +0800

    404

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -4,14 +4,18 @@
       <el-col :span="6"></el-col>
 
       <el-col :span="12">
-        <el-image
-          title="404"
-          :src="require('@/assets/404.png')"
-        ></el-image>
-        <el-link
-          href="/"
-          type="primary"
-        >回到首页</el-link>
+        <el-row>
+          <el-image
+            title="404"
+            :src="require('@/assets/404.png')"
+          ></el-image>
+        </el-row>
+        <el-row>
+          <el-link
+            href="/"
+            type="primary"
+          >回到首页</el-link>
+        </el-row>
       </el-col>
 
       <el-col :span="6"></el-col>
